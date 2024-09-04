ff5ee78414eb6ea699a6284593e2428b9aac4738
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-04 15:23:35 +0200

    access-control-allow-origin

--- API/Program.cs
+++ API/Program.cs
@@ -9,9 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>( opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
