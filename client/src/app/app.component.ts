170ba63caf0e272954ccab5c507be4c87c6bfccd
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-06 17:05:42 +0200

    add register component and functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+registerMode = false;
+registerToggle(){
+  this.registerMode = !this.registerMode
+}
+}
