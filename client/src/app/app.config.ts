c97fb427230047c4675613e0cf156d63b331f640
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-07 12:06:10 +0200

    Add routes and ngx-toastr

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
