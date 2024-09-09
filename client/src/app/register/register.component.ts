64ef3a816d25694376f0c2be95ead756b5a96fa0
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-09 10:31:33 +0200

    Adding route guard and bootswatch theme

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +12,9 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService)
+  private toastr = inject(ToastrService)
   model: any = {};
-  // @Input()
-  // usersFormHomeComponent:any;
-  // @Output()
-  // cancelRegister = new EventEmitter();
+
   cancelRegister = output<boolean>();
   usersFormHomeComponent = input.required<any>();
 
@@ -25,7 +24,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
 
     })
   }
