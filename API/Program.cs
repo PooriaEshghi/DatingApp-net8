c42cea8f385775ba0d472e648066fa50c6a6c62b
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-10 12:34:44 +0200

    Adding error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,20 +5,26 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 export const routes: Routes = [
-  {path:'', component: HomeComponent},
+  { path: '', component: HomeComponent },
   {
-    path:'',
+    path: '',
     runGuardsAndResolvers: 'always',
     canActivate: [authGuard],
-    children:[
-      {path:'members', component: MemberListComponent},
-      {path:'members/:id', component: MemberDetailComponent},
-      {path:'lists', component: ListsComponent},
-      {path:'messages', component: MessagesComponent},
-    ]
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
   },
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
 
-  {path:'**', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
