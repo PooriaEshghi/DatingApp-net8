81d39fa409dcbc3bd2f0ca97c9a6fbf4c0270728
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-17 10:55:25 +0200

    Adding photo gallery

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,29 +2,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/members';
-import { AccountService } from './account.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
-private http = inject(HttpClient)
-private accountService = inject(AccountService)
-baseUrl = environment.apiUrl
-  constructor() { }
-  getMembers(){
-
-    return this.http.get<Member[]>(`${this.baseUrl}users`, this.getHttpOptions())
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  constructor() {}
+  getMembers() {
+    return this.http.get<Member[]>(`${this.baseUrl}users`);
   }
-  getMember(username:string){
-
-    return this.http.get<Member>(`${this.baseUrl}users/${username}`,this.getHttpOptions())
-  }
-  getHttpOptions(){
-    return {
-      headers: new HttpHeaders({
-        Authorization:`Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
+  getMember(username: string) {
+    return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 }
