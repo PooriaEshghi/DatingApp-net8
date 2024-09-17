83ff27a7f1872aea6efd1e02343d80117e89aafd
PooriaEshghi <pooriaeshghi@gmail.com>
2024-09-17 15:54:08 +0200

    Adding CanDeactivate guard rout

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,44 @@
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
+import { Member } from '../../_models/members';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+
+@Component({
+  selector: 'app-member-edit',
+  standalone: true,
+  imports: [TabsModule,FormsModule],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event:any){
+    if(this.editForm?.dirty){
+      $event.returnValue = true
+    }
+  }
+  member?: Member;
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+  private tostr = inject(ToastrService);
+  ngOnInit(): void {
+    this.loadMember()
+  }
+  loadMember(){
+    const user = this.accountService.currentUser();
+    if(!user) return;
+    this.memberService.getMember(user.username).subscribe({
+      next: member => this.member = member
+    })
+  }
+  updateMember(){
+    console.log(this.member);
+    this.tostr.success('Profile updated successfully')
+    this.editForm?.reset(this.member)
+  }
+
+}
